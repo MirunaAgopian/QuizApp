5feb27a70ac2619bb114d1f163ef68b5c39ed4f2
Miruna Agopian <agopian.miruna@yahoo.com>
2025-07-27 19:08:06 +0200

    Add quizz card layout with Bootstrap\n- Created JSON structure for quiz questions and answers\n- Created functions to display JSON objects in browser

---
+++ script.js
@@ -0,0 +1,202 @@
+let questions = [
+  {
+    "question": "Wer hat HTML erfunden?",
+    "answers": [
+      "Michael Jackson",
+      "Miruna Agopian",
+      "Tim Berners-Lee",
+      "Lady Gaga",
+    ],
+    "right_answer": 2,
+  },
+  {
+    "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+    "answers": [
+      "readonly",
+      "max",
+      "from",
+      "spellcheck",
+    ],
+    "right_answer": 0,
+  },
+  {
+    "question": "Wie wählt man alle Elemente vom Typ &lt;a&gt; mit dem Attribut title aus?",
+    "answers": [
+      "a[title]{...}",
+      "a > title {...}",
+      "a.title {...}",
+      "a=title {...}",
+    ],
+    "right_answer": 0,
+  },
+  {
+    "question": "Wie definiert man eine Variable in JavaScript?",
+    "answers": [
+      "let 100 = rate;",
+      "100 = let rate;",
+      "rate = 100;",
+      "let rate = 100;",
+    ],
+    "right_answer": 3,
+  },
+  {
+    "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
+    "answers": [
+      "Text Fett",
+      "Container",
+      "Ein Link",
+      "Kursiv",
+    ],
+    "right_answer": 2,
+  },
+  {
+    "question": "Wie bindet man eine Webseite in eine andere ein?",
+    "answers": [
+      "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+      "&lt;iframe&gt;",
+      "&lt;frame&gt;",
+      "&lt;frameset&gt;",
+    ],
+    "right_answer": 3,
+  },
+   {
+    "question": "Welches Attribut muss man in einem &lt;a&gt; hinzufügen?",
+    "answers": [
+      "src",
+      "alt",
+      "max",
+      "href",
+    ],
+    "right_answer": 3,
+  }  
+];
+
+let currentQuestion = 0;
+let rightNrOfQuestions = 0;
+let audioSuccess = new Audio('./audio/correct.wav');
+let audioFail = new Audio('./audio/wrong.wav');
+
+function render(){
+    let questionTotal = document.getElementById('all_questions');
+    questionTotal.innerHTML = questions.length;
+
+    showCurrentQuestion();
+}
+
+//Like so should a good refractored funcion look!
+function showCurrentQuestion(){
+  if(gameIsOver()){
+    showEndScreen();
+  } else {
+    updateProgressBar();
+    showQuestionScreen();
+  }
+}
+
+function gameIsOver(){
+ return currentQuestion >= questions.length;
+}
+
+function showEndScreen(){
+    document.getElementById('quizz_end').classList.remove('d-none');
+    document.getElementById('quizz_body').classList.add('d-none');
+    document.getElementById('right_nr_questions').innerHTML = rightNrOfQuestions;
+    document.getElementById('total_questions').innerHTML = questions.length;
+    document.getElementById('quizz_img').src = "./assets/win.webp";
+}
+
+function showQuestionScreen(){
+    let question = questions[currentQuestion];
+    document.getElementById('question_text').innerHTML = question.question;
+    document.getElementById('answer_0').innerHTML = question.answers[0];
+    document.getElementById('answer_1').innerHTML = question.answers[1];
+    document.getElementById('answer_2').innerHTML = question.answers[2];
+    document.getElementById('answer_3').innerHTML = question.answers[3];
+}
+
+function updateProgressBar(){
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress_bar').innerHTML = `${percent}%`;
+    document.getElementById('progress_bar').style = `width: ${percent}%;`;
+}
+
+function evaluateAnswer(selection){ //selection = parameter from each div class = answer-card
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1) ; //to selec the index, not the element
+    let idOfRightAnswer =`answer_${question.right_answer}`; //in this way I pass a DOM id to a variable
+    
+    if(rightAnswerSelected(selectedQuestionNumber)) {
+        document.getElementById(selection).parentNode.classList.add('bg-success-subtle');
+        audioSuccess.play();
+        rightNrOfQuestions++;
+        
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger-subtle');
+        //.parentNode selects the parent element, so the parent <div>
+
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success-subtle');
+        //if wrong answer is clicked, highlight right answer
+
+        audioFail.play();
+    }
+    document.getElementById("next_btn").disabled = false;
+}
+
+function rightAnswerSelected(selectedQuestionNumber){
+  let question = questions[currentQuestion];
+  return selectedQuestionNumber == question.right_answer;
+}
+
+function changeQuestion(){
+  currentQuestion++;
+  document.getElementById("next_btn").disabled = true;
+  resetAnswerColor();
+  changeNrOfCurrenrQuestion();
+  showCurrentQuestion();
+}
+
+function resetAnswerColor(){
+  document.getElementById('answer_0').parentNode.classList.remove('bg-danger-subtle');
+  document.getElementById('answer_0').parentNode.classList.remove('bg-success-subtle');
+
+  document.getElementById('answer_1').parentNode.classList.remove('bg-danger-subtle');
+  document.getElementById('answer_1').parentNode.classList.remove('bg-success-subtle');
+
+  document.getElementById('answer_2').parentNode.classList.remove('bg-danger-subtle');
+  document.getElementById('answer_2').parentNode.classList.remove('bg-success-subtle');
+
+  document.getElementById('answer_3').parentNode.classList.remove('bg-danger-subtle');
+  document.getElementById('answer_3').parentNode.classList.remove('bg-success-subtle');
+}
+
+function changeNrOfCurrenrQuestion(){
+  let currentQuestionNr =  document.getElementById('current_question_nr');
+  let currentValue = parseInt(currentQuestionNr.innerText, 10);
+
+    currentValue++;
+    currentQuestionNr.innerText = currentValue;
+
+}
+
+//Thhings about parseInt():
+//10 is a radix, as parseInt() takes 2 arguments (sting, radix)
+//a radix tells JS what number system to interpret the tring in
+// 10 means decimal.
+// 16 means hexadecimal.
+// 2 means binary.
+// 8 means octal.
+
+function restartGame(){
+  document.getElementById('quizz_img').src = "./assets/quiz_img.jpg";
+  document.getElementById('quizz_end').classList.add('d-none');
+  document.getElementById('quizz_body').classList.remove('d-none');
+
+  currentQuestion = 0;
+  rightNrOfQuestions = 0;
+  
+  let currentQuestionNr =  document.getElementById('current_question_nr');
+  currentQuestionNr.innerText = 1;
+
+  render();
+}
